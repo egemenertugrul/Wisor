51c6f19346c5f7cdb5d784df0076e630da4b63a0
egemenertugrul <egemenertugrul@outlook.com>
2023-08-24 16:12:11 +0300

    logging config, heartbeat, dynamic fps adjustment, bi-directional pipeline fixes

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 from owxr.core import Core
 import argparse
+import logging
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog="OpenWiXR", description="OpenWirelessXR-Core")
     parser.add_argument("--fps", type=int, default=90)
     parser.add_argument("--disable-renderer", action="store_true")
     parser.add_argument("-d", "--display", type=str, default=":0")
-    parser.add_argument("-v", "--verbose", action="store_true")
+    parser.add_argument("--verbose", "-v", action="count", default=1)
 
     args = parser.parse_args()
-    if args.verbose:
-        print("Arguments: ")
-        for key, value in vars(args).items():
-            print(f"\t{key}: {value}")
+
+    args.verbose = 40 - (10 * args.verbose) if args.verbose > 0 else 0
+    print(f"Verbose level: {logging.getLevelName(args.verbose)}")
+    logging.basicConfig(
+        level=args.verbose,
+        format="%(asctime)s %(levelname)s: %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
 
     core = Core(args=args)
     core.start()
--- owxr/core.py
+++ owxr/core.py
@@ -8,7 +8,7 @@ import numpy as np
 from pathlib import Path
 from queue import Queue
 
-from owxr.modules.clock import Clock
+# from owxr.modules.clock import Clock
 
 # from owxr.modules.sensor_mpu9250 import MPU9250
 from owxr.modules.sensor_sensehat import SenseHat
@@ -18,13 +18,14 @@ from owxr.modules.wifi import Wifi
 
 class Core:
     status = None
+    heartbeat_timeout_secs = 2
 
     def __init__(self, args):
         self.args = args
         self.FPS = self.args.fps
         os.environ["DISPLAY"] = self.args.display
 
-        self.clock = Clock(self.FPS)
+        # self.clock = Clock(self.FPS)
 
         self.isRunning = False
         self.isRendererReady = False
@@ -44,11 +45,26 @@ class Core:
         self.commands_dict = {
             "Begin": self.set_renderer_ready,
             "QRScan": self.qrScanner.start_scanning,
+            "FPSUpdate": self.update_fps,
+            "Heartbeat": self.heartbeat,
             "Shutdown": self.shutdown,
         }
 
+        self.heartbeat_remaining = (
+            self.heartbeat_timeout_secs * 5
+        )  # Give some time to launch
+
+    def heartbeat(self):
+        logging.debug(f"Heartbeat received from renderer.")
+        self.heartbeat_remaining = self.heartbeat_timeout_secs
+
+    def update_fps(self, new_fps):
+        logging.debug(f"Updating FPS to: {new_fps}")
+        self.FPS = int(new_fps)
+
     def set_renderer_ready(self):
         self.isRendererReady = True
+        self.out_message_queue.put(self.update_status())
 
     def get_wifi_state(self):
         return len(self.wifi.get_wifi_networks()) > 0
@@ -102,28 +118,32 @@ class Core:
         )
 
         # Start the renderer program as a child process
-        if not self.args.disable_renderer:
-            renderer_path_bin = Path(os.path.normpath("../OpenWiXR-Renderer")).resolve()
-            renderer_filepath = renderer_path_bin.joinpath("openwixr_renderer")
-            if not renderer_filepath.is_file():
-                logging.error(
-                    f"Renderer executable at {renderer_filepath} does not seem to exist.\nMake sure to build the renderer."
-                )
-                return
 
-            self.renderer_process = subprocess.Popen(
-                renderer_filepath, cwd=renderer_path_bin
+        self.renderer_wd = Path(os.path.normpath("../OpenWiXR-Renderer")).resolve()
+        self.renderer_full_filepath = self.renderer_wd.joinpath("openwixr_renderer")
+        if not self.renderer_full_filepath.is_file():
+            logging.error(
+                f"Renderer executable at {self.renderer_full_filepath} does not seem to exist.\nMake sure to build the renderer."
             )
+            return
+        
+        self.start_renderer()
 
         # endregion
-
-        self.out_message_queue.put(self.update_status())
-        time.sleep(1)
 
         self.isRunning = True
 
         # Run the raylib loop
         while self.isRunning:
+            if self.heartbeat_remaining <= 0 and not self.args.disable_renderer:
+                logging.warn(
+                    f"No heartbeat from the renderer process for the last {self.heartbeat_timeout_secs} seconds."
+                )
+                self.isRendererReady = False
+                self.heartbeat_remaining = self.heartbeat_timeout_secs * 5
+                self.start_renderer()
+                logging.warn(f"Restarting..")
+
             # Check if the pipe is ready for reading or writing
             read_ready, write_ready, _ = select.select(
                 [self.to_core_pipe_fd], [self.to_renderer_pipe_fd], [], 0
@@ -131,29 +151,40 @@ class Core:
 
             if read_ready:
                 # Read the message from the renderer
-                message = os.read(self.to_core_pipe_fd, 1024)
-                messages = list(
-                    filter(lambda s: len(s) > 0, message.decode().split("\0"))
-                )
-                for m in messages:
-                    if m:
-                        try:
-                            message = json.loads(m)
-                        except Exception as e:
-                            logging.error(e)
-                            continue
-
-                        if message["type"] == "Command":
+                message = os.read(self.to_core_pipe_fd, 4096)
+                try:
+                    messages = list(
+                        filter(lambda s: len(s) > 0, message.decode().split("\n"))
+                    )
+                except Exception as e:
+                    logging.exception(e)
+                else:
+                    for m in messages:
+                        if m:
                             try:
-                                cmd = self.commands_dict[message["data"]]
-                            except KeyError as err:
-                                logging.error(err)
-                            else:
-                                cmd()
+                                message = json.loads(m)
+                            except Exception as e:
+                                logging.exception(e)
+                                continue
 
-                        logging.debug("==CORE== Received message from Renderer:")
-                        logging.debug("\tType:", message["type"])
-                        logging.debug("\tData:", message["data"])
+                        try:
+                            logging.info("==CORE== Received message from Renderer:")
+                            message_type = message["type"]
+                            logging.info(f"\tType: {message_type}")
+
+                            cmd = self.commands_dict.get(message_type)
+
+                            if cmd:
+                                logging.info(f"\tData: {message.get('data')}")
+                                data = message.get("data")
+                        except KeyError as err:
+                            logging.exception(err)
+                        finally:
+                            if cmd:
+                                if data:
+                                    cmd(data)
+                                else:
+                                    cmd()
 
             if write_ready:
                 if not self.out_message_queue.empty():
@@ -168,13 +199,35 @@ class Core:
                         )
 
             self.update()
-            time.sleep(float(1 / self.FPS))
+            sleepTime = float(1 / self.FPS)
+            if self.isRendererReady:
+                self.heartbeat_remaining -= sleepTime
+
+            time.sleep(sleepTime)
             # self.clock.sleep()
 
         # Close the pipe
         os.close(self.to_core_pipe_fd)
         os.close(self.to_renderer_pipe_fd)
-        self.renderer_process.wait()
+
+        if self.renderer_process:
+            self.renderer_process.wait()
+
+    def start_process(self, wd, executable_full_filepath):
+        process = subprocess.Popen(executable_full_filepath, cwd=wd)
+        return process
+
+    def start_renderer(self):
+        if self.renderer_process:
+            if self.renderer_process and isinstance(
+                self.renderer_process, subprocess.Popen
+            ):
+                self.renderer_process.kill()
+
+        if not self.args.disable_renderer:
+            self.renderer_process = self.start_process(
+                self.renderer_wd, self.renderer_full_filepath
+            )
 
     def update(self):
         if self.qrScanner.process:
@@ -191,5 +244,5 @@ class Core:
             )
 
     def shutdown(self):
-        logging.log("Shutting down..")
+        logging.info("Shutting down..")
         self.isRunning = False
