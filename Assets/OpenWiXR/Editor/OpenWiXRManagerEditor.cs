d2e396d13e1e6d65d62eac3c161cbdbb581ed592
egemenertugrul <egemenertugrul@yandex.com>
2023-11-06 20:35:04 +0300

    Solved Editor related bugs

--- Assets/OpenWiXR/Editor/OpenWiXRManagerEditor.cs
+++ Assets/OpenWiXR/Editor/OpenWiXRManagerEditor.cs
@@ -16,8 +16,6 @@ namespace OpenWiXR
         {
             serializedObject.Update();
 
-            OpenWiXRManager manager = (OpenWiXRManager)target;
-
             // -- LOGO
             Texture2D logoTexture = AssetDatabase.LoadAssetAtPath<Texture2D>("Assets/OpenWiXR/Editor/Images/owxr_small_logo_reddot.png");
             if(!logoTexture)
@@ -44,9 +42,9 @@ namespace OpenWiXR
                 GUILayout.Label("Enter the network information of the client. If Raspberry Pi, avoid using hostnames like 'raspberrypi.local' as they won't be resolved here.", labelStyle);
                 GUILayout.Space(5);
 
-                manager.IP = EditorGUILayout.TextField("IP", manager.IP);
-                manager.port = (uint)EditorGUILayout.IntField("Port", (int)manager.port);
-                manager.AutoconnectInterval = (uint)EditorGUILayout.IntField("Autoconnect Interval", (int)manager.AutoconnectInterval);
+                EditorUtilities.PropertyField(serializedObject, "IP");
+                EditorUtilities.PropertyField(serializedObject, "port");
+                EditorUtilities.PropertyField(serializedObject, "AutoconnectInterval");
 
                 EditorGUI.indentLevel--;
             }
@@ -60,8 +58,9 @@ namespace OpenWiXR
             if (showVideoStreamerSettings)
             {
                 EditorGUI.indentLevel++;
-                manager.VideoStreamerConfig = EditorGUILayout.ObjectField(manager.VideoStreamerConfig, typeof(VideoStreamerConfig)) as VideoStreamerConfig;
-                manager.VideoStreamerConfig_IdenticalIP = EditorGUILayout.Toggle("Identical IP", manager.VideoStreamerConfig_IdenticalIP);
+                EditorUtilities.PropertyField(serializedObject, "VideoStreamerConfig");
+                SerializedProperty identicalIP = serializedObject.FindProperty("VideoStreamerConfig_IdenticalIP");
+                identicalIP.boolValue = EditorGUILayout.Toggle("Identical IP", identicalIP.boolValue);
                 EditorGUI.indentLevel--;
             }
             EditorGUILayout.EndFoldoutHeaderGroup();
@@ -74,7 +73,7 @@ namespace OpenWiXR
             if (showVideoReceiverSettings)
             {
                 EditorGUI.indentLevel++;
-                manager.VideoReceiverConfig = EditorGUILayout.ObjectField(manager.VideoReceiverConfig, typeof(VideoReceiverConfig)) as VideoReceiverConfig;
+                EditorUtilities.PropertyField(serializedObject, "VideoReceiverConfig");
                 EditorGUI.indentLevel--;
             }
             EditorGUILayout.EndFoldoutHeaderGroup();
@@ -82,69 +81,43 @@ namespace OpenWiXR
 
             EditorUtilities.Separator();
 
-            //manager.OpenWiXROpMode = (OpenWiXRManager.OpMode)EditorGUILayout.EnumPopup("Op Mode", manager.OpenWiXROpMode);
-            manager.PoseDriverTarget = (Transform)EditorGUILayout.ObjectField("Pose Driver Target", manager.PoseDriverTarget, typeof(Transform), true);
-
+            EditorUtilities.PropertyField(serializedObject, "PoseDriverTarget");
             EditorUtilities.PropertyField(serializedObject, "OpenWiXROpMode");
             EditorGUI.indentLevel++;
-            if (manager.OpenWiXROpMode == OpenWiXRManager.OpMode.ORIENTATION_ONLY)
+            
+            SerializedProperty opModeProperty = serializedObject.FindProperty("OpenWiXROpMode");
+            OpenWiXRManager.OpMode opMode = (OpenWiXRManager.OpMode)opModeProperty.enumValueIndex;
+
+            SerializedProperty poseDriver = serializedObject.FindProperty("PoseDriver");
+
+            if (opMode == OpenWiXRManager.OpMode.ORIENTATION_ONLY)
             {
-                //HideField("ORBSLAM3 Settings");
-                //HideField("SLAM Texture Source");
-                manager.PoseDriver = (IMUPoseDriver)EditorGUILayout.ObjectField("Active Pose Driver", FindAnyObjectByType<IMUPoseDriver>(FindObjectsInactive.Include), typeof(IMUPoseDriver), true);
+                poseDriver.objectReferenceValue = (IMUPoseDriver)EditorGUILayout.ObjectField("Active Pose Driver", FindAnyObjectByType<IMUPoseDriver>(FindObjectsInactive.Include), typeof(IMUPoseDriver), true);
             }
-            else if (manager.OpenWiXROpMode == OpenWiXRManager.OpMode.KEYBOARD_MOUSE)
+            else if (opMode == OpenWiXRManager.OpMode.KEYBOARD_MOUSE)
             {
-                manager.PoseDriver = (KeyboardMousePoseDriver)EditorGUILayout.ObjectField("Active Pose Driver", FindAnyObjectByType<KeyboardMousePoseDriver>(FindObjectsInactive.Include), typeof(KeyboardMousePoseDriver), true);
+                poseDriver.objectReferenceValue = (KeyboardMousePoseDriver)EditorGUILayout.ObjectField("Active Pose Driver", FindAnyObjectByType<KeyboardMousePoseDriver>(FindObjectsInactive.Include), typeof(KeyboardMousePoseDriver), true);
             }
-            else if(manager.OpenWiXROpMode == OpenWiXRManager.OpMode.SLAM)
+            else if(opMode == OpenWiXRManager.OpMode.SLAM)
             {
-                manager.ORBSLAM3_Settings = (ORBSLAM3Config)EditorGUILayout.ObjectField("ORBSLAM3 Settings", manager.ORBSLAM3_Settings, typeof(ORBSLAM3Config), true);
-                manager.SLAMTextureSource = (TextureSource)EditorGUILayout.ObjectField("SLAM Texture Source", manager.SLAMTextureSource, typeof(TextureSource), true);
-                manager.PoseDriver = (SLAMPoseDriver)EditorGUILayout.ObjectField("Active Pose Driver", FindAnyObjectByType<SLAMPoseDriver>(FindObjectsInactive.Include), typeof(SLAMPoseDriver), true);
+                SerializedProperty ORBSLAM3_Settings = serializedObject.FindProperty("ORBSLAM3_Settings");
+                SerializedProperty SLAMTextureSource = serializedObject.FindProperty("SLAMTextureSource");
+
+                ORBSLAM3_Settings.objectReferenceValue = (ORBSLAM3Config)EditorGUILayout.ObjectField("ORBSLAM3 Settings", ORBSLAM3_Settings.objectReferenceValue, typeof(ORBSLAM3Config), true);
+                SLAMTextureSource.objectReferenceValue = (TextureSource)EditorGUILayout.ObjectField("SLAM Texture Source", SLAMTextureSource.objectReferenceValue, typeof(TextureSource), true);
+                poseDriver.objectReferenceValue = (SLAMPoseDriver)EditorGUILayout.ObjectField("Active Pose Driver", FindAnyObjectByType<SLAMPoseDriver>(FindObjectsInactive.Include), typeof(SLAMPoseDriver), true);
             } else
             {
-                manager.PoseDriver = null;
-                DisableField("Active Pose Driver");
-                DisableField("Pose Driver Target");
+                poseDriver.objectReferenceValue = null;
+                EditorUtilities.DisableField(serializedObject, "Active Pose Driver");
+                EditorUtilities.DisableField(serializedObject, "Pose Driver Target");
             }
 
             EditorGUI.indentLevel--;
 
-
             if (EditorGUI.EndChangeCheck())
             {
                 serializedObject.ApplyModifiedProperties();
-            }
-           
-        }
-
-        private void HideField(string fieldName)
-        {
-            SerializedProperty prop = serializedObject.FindProperty(fieldName);
-            if (prop != null)
-            {
-                EditorGUI.BeginChangeCheck();
-                EditorGUILayout.PropertyField(prop, true);
-                if (EditorGUI.EndChangeCheck())
-                {
-                    serializedObject.ApplyModifiedProperties();
-                }
-            }
-        }
-        private void DisableField(string fieldName)
-        {
-            SerializedProperty prop = serializedObject.FindProperty(fieldName);
-            if (prop != null)
-            {
-                EditorGUI.BeginChangeCheck();
-                EditorGUI.BeginDisabledGroup(true); // Start disabling the field
-                EditorGUILayout.PropertyField(prop, true);
-                EditorGUI.EndDisabledGroup(); // End disabling the field
-                if (EditorGUI.EndChangeCheck())
-                {
-                    serializedObject.ApplyModifiedProperties();
-                }
             }
         }
     }
